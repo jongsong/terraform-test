ce58a05e41fe0a0c4f45bae545d21c10eef39110
Jongsong <jongsong@gmail.com>
2017-08-16 14:01:16 +0900

    Initial Version

---
+++ example.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+	access_key = "AKIAJA2CJDXSKDKP27VQ"
+	secret_key = "MvNCTeJVMwON7UYUDOJ2EzUrGqRD7HpeLhwrMuMs"
+	region = "us-east-1"
+}
+
+resource "aws_instance" "example_inst" {
+	name = "terraform-example"
+	ami = "ami-b374d5a5"
+#	ami = "ami-e537069e"
+	instance_type = "t2.micro"
+	subnet_id = "subnet-335ef77b"
+	key_name = "jskim-test"
+
+	provisioner "local-exec" {
+		command = "echo ${aws_instance.example_inst.public_ip} > ip_address.txt"
+	}
+}
+

796b9142ff7a359d2e9285a3f6f2a71e9c097275
Jongsong <jongsong@gmail.com>
2017-08-21 10:50:02 +0900

    Test #1

--- example.tf
+++ example.tf
@@ -10,9 +10,16 @@ resource "aws_instance" "example_inst" {
 	instance_type = "t2.micro"
 	subnet_id = "subnet-335ef77b"
 	key_name = "jskim-test"
-
-	provisioner "local-exec" {
-		command = "echo ${aws_instance.example_inst.public_ip} > ip_address.txt"
+	tags {
+		Name = "New Instance #1"
 	}
+}
+
+resource "aws_instance" "example_inst222" {
+	ami = "ami-b374d5a5"
+#	ami = "ami-e537069e"
+	instance_type = "t2.micro"
+	subnet_id = "subnet-335ef77b"
+	key_name = "jskim-test"
 }
 
